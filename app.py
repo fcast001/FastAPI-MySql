31a1a16af0ea4a3a83005a00535b9dae1a3fd6d5
Felipe Castillo A <fcastilloaravena@hotmail.com>
2021-11-08 00:51:53 -0300

    Termino de mi API

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from router.user import user #Desde router llamamos sus funciones de user (Modulo)
 
-app = FastAPI() #Esta es mi aplicación
+#Esta es mi aplicación
+app = FastAPI(
+        title="Mi primera API desde fastapi",
+        description="Es mi primer desarrollo de FastAPI",
+        version="0.0.0.1",
+        openapi_tags=[{
+            "name" : "Users",
+            "description":"Metodos de Eliminacion de Usuarios"
+        }]
+) 
 
 app.include_router(user) #Incluimos las rutas que vienen desde user
--- router/user.py
+++ router/user.py
@@ -1,28 +1,58 @@
-from fastapi import APIRouter #Definir Sub Rutas
-from config.db import conn # Importar la bbdd conexion
-from models.user import users #Importar el esquema, es la tabla
-from schemas.user import User #Importamos el esquema de usuarios
-from cryptography.fernet import Fernet #Para encriptar password
+from typing import List
+from fastapi import APIRouter, Response, status  # Definir Sub Rutas
+from config.db import conn  # Importar la bbdd conexion
+from models.user import users  # Importar el esquema, es la tabla
+from schemas.user import User  # Importamos el esquema de usuarios
+from cryptography.fernet import Fernet  # Para encriptar password
+# Para responder el estado de la peticion
+from starlette.status import HTTP_204_NO_CONTENT
 
-key = Fernet.generate_key() #Iniciamos Fernet
-f = Fernet(key) #Creamos la funcion
-user = APIRouter() #Ejecutamos el API
 
-#Decorador
+key = Fernet.generate_key()  # Iniciamos Fernet
+f = Fernet(key)  # Creamos la funcion
+user = APIRouter()  # Ejecutamos el API
 
-##Rutas tipicas de un CRUD
-@user.get("/users") #Para generar conexion a la BBDD es necesario configurar en archivo config.py
+# Decorador
+
+# Rutas tipicas de un CRUD
+
+
+# Para generar conexion a la BBDD es necesario configurar en archivo config.py
+# response_model=list[User] = Que es lo que necesita 
+# tags=["Users"] Agrupamos los metodos
+@user.get("/users",response_model=list[User],tags=["Users"])
 def get_users():
     return conn.execute(users.select()).fetchall()
 
-@user.post("/users")
-def create_user(user: User):
-    new_user = {"name": user.name, "email": user.email}#Creamos el diccionario
-    new_user["password"] = f.encrypt(user.password.encode("utf-8"))#Encriptamos la password
-    result = conn.execute(users.insert().values(new_user))#Ejecutamos el inser
-    return conn.execute(users.select().where(users.c.id == result.lastrowid)).first()#Imprimimos el resultado para el cliente
-    
 
-@user.get("/users/{id}") #Seleccionamos registro con parametro ingresado por el user
-def get_users(id:str):
-    return conn.execute(users.select().where(users.c.id == id)).first()#Imprimimos el resultado para el cliente
+@user.post("/users",response_model=User,tags=["Users"])
+def create_user(user: User):
+    # Creamos el diccionario
+    new_user = {"name": user.name, "email": user.email}
+    new_user["password"] = f.encrypt(
+        user.password.encode("utf-8"))  # Encriptamos la password
+    result = conn.execute(users.insert().values(
+        new_user))  # Ejecutamos el inser
+    # Imprimimos el resultado para el cliente
+    return conn.execute(users.select().where(users.c.id == result.lastrowid)).first()
+
+
+# Seleccionamos registro con parametro ingresado por el user
+@user.get("/users/{id}",response_model=list[User],tags=["Users"])
+def get_users(id: str):
+    # Imprimimos el resultado para el cliente
+    return conn.execute(users.select().where(users.c.id == id)).first()
+
+
+@user.delete("/delete/{id}", status_code=HTTP_204_NO_CONTENT,tags=["Users"])  # Eliminamos el registro ingresado
+def delete_users(id: str):
+    # Imprimimos el resultado para el cliente
+    conn.execute(users.delete().where(users.c.id == id))
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@user.put("/update/{id}",response_model=User,tags=["Users"])  # Eliminamos el registro ingresado
+def update_users(id: str, user: User):
+    conn.execute(users.update().values(name=user.name, email=user.email, password=f.encrypt(
+        user.password.encode("utf-8"))).where(users.c.id == id))  # Imprimimos el resultado para el cliente
+    return Response(status_code=HTTP_204_NO_CONTENT)
